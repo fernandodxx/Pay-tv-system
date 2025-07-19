91ee95d0bfd4500d6ca15a8547afb6bb2762feac
fernandodxx <fernandosillvasantoss@gmail.com>
2025-07-19 20:36:48 -0300

    Add controller customer new create

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,18 @@
+class CustomersController < ApplicationController
+  def new
+    @customer = Customer.new
+  end
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :age)
+  end
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  validates :nome, presence: true
-  validates :idade, presence: true, numericality: { greater_than_or_equal_to: 18 }
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 18 }
 end
